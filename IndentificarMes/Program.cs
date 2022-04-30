4b283729df7e111408aa3c69b90ae6fc0dbbc269
SebastianGtr <tanoriruizs@gmail.com>
2022-04-30 13:17:17 -0700

    Agregar archivos de proyecto.

---
+++ IndentificarMes/Program.cs
@@ -0,0 +1,12 @@
+﻿//IdentificarMes();
+
+static void IdentificarMes(int mes) {
+    string[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+
+    //Console.WriteLine($"El mes con numero " + mes.ToString() + "es" + meses[(mes - 1)]);
+    Console.WriteLine($"El mes con numero {mes} es {meses[(mes - 1)]}");
+}
+Console.WriteLine("Ingrese el numero del mes: ");
+int mes_Recibido = int.Parse(Console.ReadLine());
+IdentificarMes(mes_Recibido);
+
